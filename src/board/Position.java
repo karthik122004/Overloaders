edf127f76bc1fd81bf47e0eb3fcf670eafb70cca
Karthik Matli <karthikarthik4282@gmail.com>
2025-10-14 14:09:12 -0500

    modified changes to board/Position.Java and abstract class pieces/Piece.java

--- src/board/Position.java
+++ src/board/Position.java
@@ -1,4 +1,34 @@
 package board;
 
 public class Position {
+    private int row;
+    private int col;
+    public Position(int row, int col) {
+        if(row < 0||row >7||col < 0||col >7){
+            throw new IllegalArgumentException("Out of bounds");
+        }
+        this.row = row;
+        this.col = col;
+    }
+    public int getRow() {
+        return row;
+    }
+    public int getCol() {
+        return col;
+    }
+    public static Position fromAlgebraic(String s){
+        char f = Character.toUpperCase(s.charAt(0));
+        char r = s.charAt(1);
+        int col = f - 'A';
+        int row = 8 - (r-'0');
+        return new Position(row, col);
+    }
+    public String toString(){
+        char file = (char)('A' + col);
+        char rank = (char)('8' - row);
+        return "" + file + rank;
+    }
+
+
+
 }
--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -1,4 +1,30 @@
 package pieces;
+import board.Board;
+import board.Position;
 
-public class Piece {
+import java.util.List;
+
+public abstract class Piece {
+    protected String color ;
+    protected Position position;
+    public Piece (String color, Position position)
+    {
+        this.color = color;
+        this.position = position;
+    }
+    public String getColor() {
+        return color;
+    }
+    public Position getPosition() {
+        return position;
+    }
+    public abstract List<Position> possibleMoves(Board board);
+    public void move(Position newposition){
+        position = newposition;
+    }
+    public String code(){
+        String pieceCode = color.equals("white") ? "w":"b";
+        return pieceCode + pieceLetter();
+    }
+    protected abstract String pieceLetter();
 }
