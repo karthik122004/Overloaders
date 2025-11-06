69096831f09fff874ea31b096aa7fce571d5354c
SalamElbahri <salambhr8@gmail.com>
2025-11-05 20:14:25 -0600

    Add basic Chess FRame.java Structure ( 800x800, tittle , close  oepration)

--- Phase2/src/ui/ChessFrame.java
+++ Phase2/src/ui/ChessFrame.java
@@ -1,4 +1,26 @@
 package ui;
 
-public class ChessFrame {
+import javax.swing.JFrame;
+
+public class ChessFrame extends JFrame {
+    /* *
+     * Consctoctor for chessFrame- Basic Structure only
+     */
+     public ChessFrame()
+
+{
+
+     setTitle("Chess Game - Phase 2");
+
+     setSize( 800, 800);
+     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     setLocationRelativeTo(null);
+
+
+
+
 }
+
+
+}
+
